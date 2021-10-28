0bf528c204a4fe672cdf8a2d4a1bf708bb669419
Michael DoHo <michaeldoho@hotmail.com>
2021-10-28 18:51:04 -0400

    figuring out firebase auth error

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.css';
+import Chat from './components/Chat';
+import SignIn from './components/SignIn';
+import { auth } from './firebase.js'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyC-j2FV-B-Z4fus8NF1LfqSNXn2441t-3Q",
-  authDomain: "discord-clone-65273.firebaseapp.com",
-  projectId: "discord-clone-65273",
-  storageBucket: "discord-clone-65273.appspot.com",
-  messagingSenderId: "615933350649",
-  appId: "1:615933350649:web:7b243a72194f0ef7b0c828",
-  measurementId: "G-21VQF4D0G4"
-})
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
-    <div className="App">
-      discord
-    </div>
+    <>
+      <SignIn />
+      <Chat />
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyC-j2FV-B-Z4fus8NF1LfqSNXn2441t-3Q",
+    authDomain: "discord-clone-65273.firebaseapp.com",
+    projectId: "discord-clone-65273",
+    storageBucket: "discord-clone-65273.appspot.com",
+    messagingSenderId: "615933350649",
+    appId: "1:615933350649:web:7b243a72194f0ef7b0c828",
+    measurementId: "G-21VQF4D0G4"
+})
+
+const db = firebaseApp.firestore()
+
+const auth = firebase.auth()
+
+export { db, auth }
