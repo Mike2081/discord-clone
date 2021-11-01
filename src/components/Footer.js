1f52c1a2c4a7e743dcc40c10e15a36a64170013c
Ted Dino <ted23@tutanota.com>
2021-11-01 23:43:01 +0800

    add sign in page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  const getCurrYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>© All Rights Reserved. {getCurrYear}</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import firebase from 'firebase/compat/app';
-import {auth} from '../firebase.js'
+import React from "react";
+import firebase from "firebase/compat/app";
+import { auth } from "../firebase.js";
+import Footer from "./Footer.js";
 
 function SignIn() {
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
 
-    function signInWithGoogle() {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        auth.signInWithPopup(provider)
-    }
-
-    return (
-        <div>
-            <button onClick={signInWithGoogle} >Sign in with google</button>
-        </div>
-    )
+  return (
+    <section>
+      <div className="sign-in">
+        <h1>Sign in to join the chat</h1>
+        <button className="sign-in__button" onClick={signInWithGoogle}>
+          Sign in with google
+        </button>
+      </div>
+      <Footer />
+    </section>
+  );
 }
 
-export default SignIn
+export default SignIn;
