ae2aa96ceb63682543496c71515ac846234af7fc
Ted Dino <ted23@tutanota.com>
2021-11-11 15:15:47 +0800

    fix warning

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,30 +1,49 @@
 import React from "react";
 import "../styles/SideBar.css";
-import LogOut from "./LogOut";
+import LogOut from "./Logout";
 import { auth } from "./firebase";
 
-function SideBar(props) {
-const {displayName} = auth.currentUser;
+function SideBar() {
+  const { displayName } = auth.currentUser;
 
   return (
     <div className="SideBar">
       <img
         className="SideBar__ProPic"
         src="../images/profile.jpg"
-        alt="computer cartoon guy"/>
+        alt="computer cartoon guy"
+      />
       <div className="SideBar__Heading">DoHoCord</div>
-      <div className="SideBar__subHead">
-        Kinda like discord, but janky
-      </div>
+      <div className="SideBar__subHead">Kinda like discord, but janky</div>
       <div className="SideBar__logos">
-        <a href ="https://www.instagram.com/doh0/" target="_blank">
-          <img className="SideBar__logos__icons" src="../images/Instagram.svg" alt="instagram logo"/>
+        <a
+          href="https://www.instagram.com/doh0/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            className="SideBar__logos__icons"
+            src="../images/Instagram.svg"
+            alt="instagram logo"
+          />
         </a>
-        <a href ="https://github.com/Mike2081" target="_blank">
-          <img className="SideBar__logos__icons" src="../images/GitHub.svg" alt="instagram logo"/>
+        <a href="https://github.com/Mike2081" target="_blank" rel="noreferrer">
+          <img
+            className="SideBar__logos__icons"
+            src="../images/GitHub.svg"
+            alt="instagram logo"
+          />
         </a>
-        <a href ="https://www.linkedin.com/in/michaeldoho/" target="_blank">
-        <img className="SideBar__logos__icons" src="../images/LinkedIn.svg" alt="instagram logo"/>
+        <a
+          href="https://www.linkedin.com/in/michaeldoho/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="SideBar__logos__icons"
+            src="../images/LinkedIn.svg"
+            alt="instagram logo"
+          />
         </a>
       </div>
       <div>Signed in as:</div>
