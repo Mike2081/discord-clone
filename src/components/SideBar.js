18ceb00af8f9f602f21f72f55021a564a63e6ea4
Ted Dino <ted23@tutanota.com>
2021-11-15 17:08:57 +0800

    style: add mobile layout

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,74 +8,86 @@ function SideBar(props) {
 
   return (
     <div className="SideBar">
-      <div className="SideBar__Heading">MintCord</div>
-      <div className="SideBar__subHead">Kinda like discord, but janky</div>
-      <div className="SideBar__devsTitle">Project Developers</div>
-
-      <div className="SideBar__logos">
-        <img
-          className="SideBar__devsBox__ProPic"
-          src="../images/profile.jpg"
-          alt="computer cartoon guy"
-        />
-
-        <div>Michael</div>
-        <a
-          href="https://www.instagram.com/doh0/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="SideBar__logos__icons"
-            src="../images/Instagram.svg"
-            alt="instagram logo"
-          />
-        </a>
-        <a href="https://github.com/Mike2081" target="_blank" rel="noreferrer">
-          <img
-            className="SideBar__logos__icons"
-            src="../images/GitHub.svg"
-            alt="instagram logo"
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/michaeldoho/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="SideBar__logos__icons"
-            src="../images/LinkedIn.svg"
-            alt="instagram logo"
-          />
-        </a>
+      <div className="SideBar__projectInfo">
+        <h1 className="SideBar__project_name">MintCord</h1>
+        <p className="SideBar__project__desc">Kinda like discord, but janky</p>
       </div>
-      <div className="SideBar__logos">
-        <img
-          className="SideBar__devsBox__ProPic"
-          src="../images/ted.jpg"
-          alt="my ugly face"
-        />
-        <div>Ted</div>
-        <a href="https://github.com/ted-dino" target="_blank" rel="noreferrer">
+      <h2 className="SideBar__devsTitle">Project Developers</h2>
+      <div className="SideBar__devs">
+        <div className="SideBar__logos">
           <img
-            className="SideBar__logos__icons"
-            src="../images/GitHub.svg"
-            alt="instagram logo"
+            className="SideBar__devsBox__ProPic"
+            src="../images/profile.jpg"
+            alt="computer cartoon guy"
           />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/ted-dino-967305222/"
-          target="_blank"
-          rel="noreferrer"
-        >
+
+          <div>Michael</div>
+          <a
+            href="https://www.instagram.com/doh0/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="SideBar__logos__icons"
+              src="../images/Instagram.svg"
+              alt="instagram logo"
+            />
+          </a>
+          <a
+            href="https://github.com/Mike2081"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="SideBar__logos__icons"
+              src="../images/GitHub.svg"
+              alt="instagram logo"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/michaeldoho/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="SideBar__logos__icons"
+              src="../images/LinkedIn.svg"
+              alt="instagram logo"
+            />
+          </a>
+        </div>
+        <div className="SideBar__logos">
           <img
-            className="SideBar__logos__icons"
-            src="../images/LinkedIn.svg"
-            alt="instagram logo"
+            className="SideBar__devsBox__ProPic"
+            src="../images/ted.jpg"
+            alt="my ugly face"
           />
-        </a>
+          <div>Ted</div>
+          <a
+            href="https://github.com/ted-dino"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="SideBar__logos__icons"
+              src="../images/GitHub.svg"
+              alt="instagram logo"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ted-dino-967305222/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="SideBar__logos__icons"
+              src="../images/LinkedIn.svg"
+              alt="instagram logo"
+            />
+          </a>
+        </div>
       </div>
+
       <div>Signed in as:</div>
       <div className="SideBar__user">{displayName}</div>
       <LogOut />
